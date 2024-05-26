3850743e592fdcb0a4a6051536b0485d5a6d0abd
alilopez <ali.lopez37@gmail.com>
2024-05-25 21:22:34 -0600

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : 'https://github.com/alilopez37/PV-3D-Cards'
+  base : '/PV-3D-Cards/'
 })
